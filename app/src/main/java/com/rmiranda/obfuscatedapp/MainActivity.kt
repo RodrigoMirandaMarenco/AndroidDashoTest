daa438b2a988aae7cd0ab4df30a0f0d32dd857f3
Rodrigo <rodrigo.kreloco@gmail.com>
2021-04-08 17:22:55 -0600

    Add basic Android project

---
+++ app/src/main/java/com/rmiranda/obfuscatedapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rmiranda.obfuscatedapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rmiranda.obfuscatedapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/rmiranda/obfuscatedapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.rmiranda.obfuscatedapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
